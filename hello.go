a62786cec37ef5ce3906e432c73de86648d89f5c
gruffner <glenn@ruffner.org>
2016-12-02 20:06:34 -0500

    test updload 3

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 import "fmt"
-
+//adding a comment
 func main() {
-   fmt.Printf("hello, world\nhello, world\nhello, world\nhello, world\n")
+   fmt.Printf("hello, world\nhello, wacky2 world\nhello, world\nhello, world\n")
 }
+//test
